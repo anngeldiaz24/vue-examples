20248f68bb85dd428c1ce4834710df6af917e870
anngeldiaz24 <angel.diaz5609@alumnos.udg.mx>
2024-04-23 13:03:52 -0600

    add: bringing all the vue components  together to create an intuitive and dynamic to-do list

---
+++ js/components/App.js
@@ -0,0 +1,25 @@
+import Assignments from "./Assignments.js";
+
+export default {
+    components: {Assignments},
+    template: `
+        <assignments></assignments>
+    `,
+
+   
+
+}
+
+
+
+
+
+/* import AppButton from "./AppButton.js";
+
+
+export default{
+    components: {
+        'app-button': AppButton
+    }
+
+} */
--- js/components/AppButton.js
+++ js/components/AppButton.js
@@ -1,13 +1,32 @@
 export default {
     template: `
-        <button class="bg-gray-200 hover:bg-gray-400 border rounded px-5 py-2" :disabled="processing" :class="{ 'cursor-not-allowed': processing }">
+        <button 
+            :class="{
+                'bg-gray-200 hover:bg-gray-400 border rounded px-5 py-2 disabled:cursos-not-allowed': true,
+                'bg-blue-200 hover:bg-blue-400': type === 'primary',
+                'bg-purple-200 hover:bg-purple-400': type === 'secondary',
+                'bg-gray-200 hover:bg-gray-400': type === 'muted',
+                'is-loading': processing
+
+            }"
+            :disabled="processing"
+        >
             <slot />
         </button>
     `,
 
-    data() {
-        return {
-            processing: false 
+    props: {
+        type: {
+            type: String,
+            default: 'primary'
+        },
+
+        processing: {
+            type: Boolean,
+            default: false
+    
         }
-    }
+    },
+
+    
 }
